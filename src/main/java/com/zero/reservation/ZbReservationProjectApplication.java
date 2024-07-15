9e3275bf9b3e830748ac0dadf7f4dca03ffc8362
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-07-15 15:20:08 +0900

    first commit

---
+++ src/main/java/com/zero/reservation/ZbReservationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.zero.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZbReservationProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZbReservationProjectApplication.class, args);
+    }
+
+}
