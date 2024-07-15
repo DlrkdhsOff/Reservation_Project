fb6f50ca5193a95a68b117138b4045320ee054dd
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-07-15 17:14:29 +0900

    회원가입

---
+++ src/main/java/com/zero/reservation/model/entity/Admin.java
@@ -0,0 +1,20 @@
+package com.zero.reservation.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity(name = "admin")
+@NoArgsConstructor
+public class Admin {
+
+    @Id
+    private String adminId;
+    private String storeName;
+    private String storeAddress;
+    private String storeInfo;
+}
---
+++ src/main/java/com/zero/reservation/repository/AccountRepository.java
@@ -0,0 +1,8 @@
+package com.zero.reservation.repository;
+
+import com.zero.reservation.model.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Member, String> {
+    boolean existsByUserId(String userId);
+}
